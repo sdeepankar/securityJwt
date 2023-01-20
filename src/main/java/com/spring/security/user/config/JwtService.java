994cdc0d383f936929b3499dbd609400cad58a13
Gears <wmsgmp@gmail.com>
2023-01-21 01:00:11 +0530

    V 1.0

--- src/main/java/com/spring/security/user/config/JwtService.java
+++ src/main/java/com/spring/security/user/config/JwtService.java
@@ -2,11 +2,14 @@ package com.spring.security.user.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -23,9 +26,17 @@ public class JwtService {
     }
 
     public String generateToken(
-            Map<String, Object> extraClaims
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
     ){
-        return null;
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token){
